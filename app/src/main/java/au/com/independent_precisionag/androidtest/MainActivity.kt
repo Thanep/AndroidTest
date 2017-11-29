51ca0b181bb497d3c06d57c2c251afa61b2cca9d
Thane Pringle <thanepringle@Thanes-iMac.lan>
2017-11-29 16:29:06 +1100

    Initial commit

---
+++ app/src/main/java/au/com/independent_precisionag/androidtest/MainActivity.kt
@@ -0,0 +1,12 @@
+package au.com.independent_precisionag.androidtest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
